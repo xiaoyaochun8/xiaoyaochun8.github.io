8a6a18e946f8360f3388a34050630f9d846b5a6e
cblvip <wjyzxhm888@163.com>
2025-09-26 15:06:45 +0800

    1

---
+++ common/js/commonBack.js
@@ -0,0 +1,11 @@
+var btn = document.createElement("BUTTON");
+var t = document.createTextNode("«返回");
+btn.appendChild(t);
+btn.style.fontSize = '1em';
+btn.style.position = 'absolute';
+btn.style.left = '0';
+btn.style.top = '0';
+btn.addEventListener('click',function(e){
+    navigation.back();
+})
+document.body.appendChild(btn);
