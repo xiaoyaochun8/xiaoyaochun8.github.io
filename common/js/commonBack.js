5ee8dd423af95d5b17afa9c0054dc0d038bbb5cd
cblvip <wjyzxhm888@163.com>
2025-10-10 17:06:45 +0800

    1

--- common/js/commonBack.js
+++ common/js/commonBack.js
@@ -1,12 +1,12 @@
-var btn = document.createElement("BUTTON");
-var t = document.createTextNode("«返回");
-btn.appendChild(t);
-btn.style.fontSize = '1em';
-btn.style.position = 'absolute';
-btn.style.left = '0';
-btn.style.top = '0';
-btn.style.zIndex = '999';
-btn.addEventListener('click',function(e){
-    navigation.back();
-})
-document.body.appendChild(btn);
+//var btn = document.createElement("BUTTON");
+//var t = document.createTextNode("«返回");
+//btn.appendChild(t);
+//btn.style.fontSize = '1em';
+//btn.style.position = 'absolute';
+//btn.style.left = '0';
+//btn.style.top = '0';
+//btn.style.zIndex = '999';
+//btn.addEventListener('click',function(e){
+//    navigation.back();
+//})
+//document.body.appendChild(btn);
